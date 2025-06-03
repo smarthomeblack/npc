d83916b2b7847a964ce208c38c1c997ce73babfc
Black <dothoidenquendi@gmail.com>
2025-06-04 06:19:59 +0700

    Create config_flow.py

---
+++ custom_components/npc/config_flow.py
@@ -0,0 +1,14 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_USERNAME
+
+DATA_SCHEMA = vol.Schema({CONF_USERNAME: str})
+
+class NPCConfigFlow(config_entries.ConfigFlow, domain="npc"):
+    async def async_step_user(self, user_input=None):
+        if user_input is None:
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+        
+        await self.async_set_unique_id(user_input[CONF_USERNAME])
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(title=user_input[CONF_USERNAME], data=user_input)
